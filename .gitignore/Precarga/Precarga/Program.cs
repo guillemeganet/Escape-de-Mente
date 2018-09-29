3a6ca9d02395f27d099be375cdc68b3a9614c174
guillemeganet <43025470+guillemeganet@users.noreply.github.com>
2018-09-29 13:38:19 -0300

    Add files via upload

---
+++ .gitignore/Precarga/Precarga/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApplication1;
+
+namespace Precarga
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+#pragma warning disable CS0436 // El tipo entra en conflicto con un tipo importado
+            Application.Run(new Precarga());
+#pragma warning restore CS0436 // El tipo entra en conflicto con un tipo importado
+        }
+    }
+}
